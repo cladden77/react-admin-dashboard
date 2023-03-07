28fa25a0a7a38ee204b817fd14d9745065eae4ac
Chris L <chris.ladden@gmail.com>
2023-03-07 11:49:30 -0600

    created profile form and validation

--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataInvoices } from "../../data/mockData";
+import Header from "../../components/Header";
 
-function Invoices() {
+const Invoices = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    { field: "name", headerName: "Name", flex: 1, cellClassName: "name-column--cell" },
+    { field: "email", headerName: "Email", flex: 1},
+    { 
+      field: "cost",
+      headerName: "Cost",
+      renderCell: (params) => {
+        <Typography color={colors.greenAccent[500]}>
+          ${params.row.cost}
+        </Typography>
+      }
+    },
+    { field: "phone", headerName: "Phone", flex: 1 },
+    { field: "date", headerName: "Date", flex: 1 },
+  ]
+
   return (
-    <div>Invoices</div>
+    <Box m="20px">
+      <Header title="Invoices" subtitle="List of current invoices" />
+
+      <Box 
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root" : {
+            border: "none"
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none" 
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300]
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none"
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400]
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700]
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          checkboxSelection 
+          rows={mockDataInvoices}
+          columns={columns}
+        />
+      </Box>
+    </Box>
   )
 }
 
-export default Invoices
+export default Invoices;
